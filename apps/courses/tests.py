3cd5463bd5dffaf1ffb97dbf95fea135f4f969a0
hzy <877695220@qq.com>
2020-07-12 22:07:11 +0800

    onteach完整项目代码

---
+++ apps/courses/tests.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+from django.db import models
+
+from apps.users.models import BaseModel
+from apps.organizations.models import Teacher
+from apps.organizations.models import CourseOrg
+
+from DjangoUeditor.models import UEditorField
